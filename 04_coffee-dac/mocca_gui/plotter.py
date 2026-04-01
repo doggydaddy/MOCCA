65472025b882fc088b4f13a1129df692b674f04a
doggydaddy <lulu.wg@gmail.com>
2026-04-01 23:36:03 +0200

    04_coffee-dac: remove brain3mm_outer.stl layer from default brain mesh stack

--- 04_coffee-dac/mocca_gui/plotter.py
+++ 04_coffee-dac/mocca_gui/plotter.py
@@ -72,7 +72,6 @@ class NetworkPlotter:
             "ambient": 0.3, "diffuse": 0.8,
             "show_edges": True, "edge_color": "#888888", "edge_opacity": 0.08,
         },                                                                                   False),
-        ("brain3mm_outer.stl", 0.04, "#D0D0D0", True,  {"specular": 0.3},                   False),
         ("brain3mm.stl",       0.02, "#D8D8D8", False, {},                                  False),
     ]
 
